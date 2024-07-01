1ab98494e101d1e21b4e5f7f7a6b61ccb2668a5e
Soifiaouir <soifiaouir@gmail.com>
2024-07-01 21:46:57 +0200

    V2 avec Search bar

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ document.addEventListener('DOMContentLoaded', () => {
   const generation = 1;
   let currentIndex = 0;
   let pokemonData = [];
+  let filteredPokemonData = []; /* tableau fitltr*/
 
-  const prevBtn = document.getElementById('prev-btn');
+  const prevBtn = document.getElementById('prev-btn');/* appel les différents éléments dans le html*/
   const nextBtn = document.getElementById('next-btn');
   const pokemonDetails = document.getElementById('pokemon-details');
+  const searchBar = document.getElementById('search-bar');
+  const searchBtn = document.getElementById('search-btn');
+
+  searchBtn.addEventListener('click', () => {
+    const query = searchBar.value.trim().toLowerCase();
+    filteredPokemonData = pokemonData.filter(pokemon => { /* Filtrage par nom*/
+      const nameMatch = pokemon.name.toLowerCase().includes(query); /* Filtrage par ID (conversion de l'ID en string pour comparaison)*/
+      const idMatch = pokemon.id.toString().includes(query);
+      return nameMatch || idMatch;
+    });
+    currentIndex = 0; /*met la valeur au pokémon id 1 (le plue proche de 0)*/ 
+    if (filteredPokemonData.length > 0) { /*met en place la condition du retout de pokémon  ou non */
+      displayPokemon(filteredPokemonData[currentIndex]);
+    } else {
+      pokemonDetails.innerHTML = '<p>Aucun Pokémon trouvé</p>';
+    }
+  });
 
   prevBtn.addEventListener('click', () => {/*permet de changer de pokémon de un en un vers la droite*/
     if (currentIndex > 0) {
